86db8320e4c7c99fa9032d8efbbe91a6710d5963
Piyush Kumar Singh <96623633+P123yu@users.noreply.github.com>
2024-05-07 18:09:13 +0530

    Update StudentModel.java

--- src/main/java/com/one_to_many/model/StudentModel.java
+++ src/main/java/com/one_to_many/model/StudentModel.java
@@ -1,10 +1,5 @@
 package com.one_to_many.model;
-
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,7 +17,7 @@ import java.util.TreeSet;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-//@JsonIgnoreProperties("address")
+
 public class StudentModel {
 
     @Id
@@ -32,15 +27,9 @@ public class StudentModel {
     private Double stuMarks;
 
 
-//    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER,mappedBy = "student")
-//  //  @JoinColumn(name = "stu_roll", referencedColumnName = "stuRoll")  // specifying the foreign key column
-//    private Set<AddressModel> address = new TreeSet<>();
-
-
-
-@JsonBackReference
-    //@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "studentModel")
-    @OneToMany(cascade=CascadeType.ALL,fetch = FetchType.EAGER, mappedBy="studentModel",orphanRemoval = false)
+    @JsonBackReference
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "studentModel")
+    //@OneToMany(cascade=CascadeType.ALL,fetch = FetchType.EAGER, mappedBy="studentModel",orphanRemoval = false)
     private Set<AddressModel> address = new TreeSet<>();
 
 }
